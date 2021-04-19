1fedcd03a2d99cc582bf9049c6dfc4fb66ea82f4
moniluiza99 <moni5265@edu.easj.dk>
2021-04-19 08:42:19 +0200

    Update validation.test.ts

--- src/validation.test.ts
+++ src/validation.test.ts
@@ -2,22 +2,22 @@ import {doesTriangleExist} from './validation';
 describe('function doesTriangleExist', ()=>{
   it('should say that a triangle does exist', ()=>{
     // arrange
-    const sideA = 1;
-    const sideB = 2;
-    const sideC = 2;
+    const sideA:number = 1;
+    const sideB:number = 2;
+    const sideC:number = 2;
     // act
-    const result = doesTriangleExist(sideA, sideB, sideC);
+    const result:boolean = doesTriangleExist(sideA, sideB, sideC);
     // assert
     expect(result).toBeTruthy();
   });
 
   it('should say that a triangle not does exist', ()=>{
     // arrange
-    const sideA = 1;
-    const sideB = 2;
-    const sideC = 4;
+    const sideA:number = 1;
+    const sideB:number = 2;
+    const sideC:number = 4;
     // act
-    const result = doesTriangleExist(sideA, sideB, sideC);
+    const result:boolean = doesTriangleExist(sideA, sideB, sideC);
     // assert
     expect(result).toBeFalsy();
   });
